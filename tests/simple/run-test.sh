bf12d8f4c80bf1cfd907e3c4b60df45b6fa4c6a8
Cotton Seed <cotton@alum.mit.edu>
2015-06-25 17:03:10 +0700

    Generate checksums in tests/simple.

--- tests/simple/run-test.sh
+++ tests/simple/run-test.sh
@@ -2,41 +2,54 @@
 
 set -ex
 
-arachne_pnr=../../bin/arachne-pnr
+seed=7
+arachne_pnr="../../bin/arachne-pnr -s $seed"
+
+rm -f txt.sum
 
 # sb_up3down5.blif
 $arachne_pnr sb_up3down5.blif -o sb_up3down5.txt
+shasum sb_up3down5.txt >> txt.sum
 icepack sb_up3down5.txt sb_up3down5.bin
 
 $arachne_pnr -l sb_up3down5.blif -o sb_up3down5_l.txt
+shasum sb_up3down5_l.txt >> txt.sum
 icepack sb_up3down5_l.txt sb_up3down5_l.bin
 
 $arachne_pnr sb_up3down5.blif -B sb_up3down5_packed.blif -o sb_up3down5.txt
 $arachne_pnr sb_up3down5_packed.blif -o sb_up3down5_packed.txt
+shasum sb_up3down5_packed.txt >> txt.sum
 icepack sb_up3down5_packed.txt sb_up3down5_packed.bin
 
 
 $arachne_pnr carry.blif -o carry.txt
+shasum carry.txt >> txt.sum
 icepack carry.txt carry.bin
 
 $arachne_pnr bram.blif -o bram.txt
+shasum bram.txt >> txt.sum
 icepack bram.txt bram.bin
 
 # FIXME don't overwrite
 # test 8k
 $arachne_pnr -d 8k sb_up3down5.blif -o sb_up3down5.txt
+shasum sb_up3down5.txt >> txt.sum
 icepack sb_up3down5.txt sb_up3down5.bin
 
 $arachne_pnr -d 8k  -l sb_up3down5.blif -o sb_up3down5_l.txt
+shasum sb_up3down5_l.txt >> txt.sum
 icepack sb_up3down5_l.txt sb_up3down5_l.bin
 
 $arachne_pnr -d 8k  sb_up3down5.blif -B sb_up3down5_packed.blif -o sb_up3down5.txt
 $arachne_pnr -d 8k  sb_up3down5_packed.blif -o sb_up3down5_packed.txt
+shasum sb_up3down5_packed.txt >> txt.sum
 icepack sb_up3down5_packed.txt sb_up3down5_packed.bin
 
 
 $arachne_pnr -d 8k  carry.blif -o carry.txt
+shasum carry.txt >> txt.sum
 icepack carry.txt carry.bin
 
 $arachne_pnr -d 8k  bram.blif -o bram.txt
+shasum bram.txt >> txt.sum
 icepack bram.txt bram.bin
