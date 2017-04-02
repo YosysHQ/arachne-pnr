e83eecb0fbcb0fe81163db4fc0db296574fe61b0
Cotton Seed <cotton@alum.mit.edu>
2017-04-02 13:46:00 -0400

    Removed BasedUllmanSet.

--- src/ullmanset.hh
+++ src/ullmanset.hh
@@ -1,17 +1,24 @@
-/* Copyright (C) 2015 Cotton Seed
+/* Copyright (c) 2015-2018 Cotton Seed
    
-   This file is part of arachne-pnr.  Arachne-pnr is free software;
-   you can redistribute it and/or modify it under the terms of the GNU
-   General Public License version 2 as published by the Free Software
-   Foundation.
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation
+   files (the "Software"), to deal in the Software without
+   restriction, including without limitation the rights to use, copy,
+   modify, merge, publish, distribute, sublicense, and/or sell copies
+   of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
    
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
    
-   You should have received a copy of the GNU General Public License
-   along with this program. If not, see <http://www.gnu.org/licenses/>. */
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE. */
 
 #ifndef PNR_ULLMANSET_HH
 #define PNR_ULLMANSET_HH
@@ -21,18 +28,17 @@
 #include <cstddef>
 #include <cassert>
 
-template<unsigned B>
-class BasedUllmanSet
+class UllmanSet
 {
   size_t n;
   std::vector<int> key;
-  BasedVector<unsigned, B> pos;
+  std::vector<unsigned> pos;
   
 public:
-  BasedUllmanSet()
+  UllmanSet()
     : n(0)
   {}
-  BasedUllmanSet(size_t cap)
+  UllmanSet(size_t cap)
     : n(0), key(cap), pos(cap)
   {}
   
@@ -94,8 +100,5 @@ public:
     return key[i];
   }
 };
-
-using UllmanSet = BasedUllmanSet<0>;
-using UllmanSet1 = BasedUllmanSet<1>;
 
 #endif
