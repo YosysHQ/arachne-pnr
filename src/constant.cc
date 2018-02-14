f2274d5f7278e6b013393954fe0607dd89768fd1
Daniel M. Weeks <dan@danweeks.net>
2018-02-14 13:29:28 -0500

    constant.cc: Include associated header first

--- src/constant.cc
+++ src/constant.cc
@@ -13,8 +13,8 @@
    You should have received a copy of the GNU General Public License
    along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
-#include "netlist.hh"
 #include "constant.hh"
+#include "netlist.hh"
 #include "chipdb.hh"
 
 #include <cassert>
