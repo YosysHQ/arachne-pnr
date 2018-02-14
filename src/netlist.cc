5e2c4e8ee62da2c0043b25cb8b9bf735838cc9c0
Daniel M. Weeks <dan@danweeks.net>
2018-02-14 13:30:15 -0500

    netlist.cc: Include associated header first

--- src/netlist.cc
+++ src/netlist.cc
@@ -13,8 +13,8 @@
    You should have received a copy of the GNU General Public License
    along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
-#include "util.hh"
 #include "netlist.hh"
+#include "util.hh"
 #include "casting.hh"
 
 #include <cassert>
